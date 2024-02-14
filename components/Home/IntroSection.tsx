f79ffe9f519f7332302001dd426b064db0862e0d
Atharva Meher <92387222+atharvam2004@users.noreply.github.com>
2024-02-14 19:40:58 +0530

    nav

--- components/Home/IntroSection.tsx
+++ components/Home/IntroSection.tsx
@@ -10,28 +10,26 @@ const poppins = Poppins({
 export default function IntroSection() {
   return (
     <div className={poppins.className}>
-      <div className="h-screen flex items-center justify-center">
+      <div className="h-screen flex  justify-center">
         {/* Use the Image component */}
         <Image
           src={Homebg}
           alt="Home Background"
           layout="fill"
           objectFit="cover"
-          className="absolute mt-[13vh]"
+          className="absolute xl:mt-[13vh] mt-[8vh]"
         />
-
-        <div className="z-10 text-white flex flex-col lg:flex-row w-screen p-6 ">
-          <div className="lg:w-3/5 lg:pr-8   pt-12">
-            <h1 className="text-4xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-black">
+        <div className="z-10 text-white flex flex-col xl:flex-row w-screen p-6   ">
+          <div className="xl:w-3/5 lg:pr-8 m-10  xl:pt-12 ">
+            <h1 className="text-4xl sm:text-4xl md:text-4xl lg:text-6xl xl:text-6xl font-bold text-black">
               Find your Tribe, <span style={{ color: "#FFA500" }}>Explore</span>
             </h1>
-            <p className="text-2xl  text-slate-600  pt-5 lg:w-4/5	">
+            <p className="text-2xl  text-slate-600  pt-5 xl:w-4/5	">
               Connect to like-minded travelers for shared adventures. Create a
               unique experience or join an experiences created by others.
             </p>
           </div>
-
-          <div className="lg:w-2/5">
+          <div className="xl:w-2/5 lg:w-[100%] ">
             <Image src={exp} alt="Explore Image" />
           </div>
         </div>
--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -4,11 +4,11 @@ export default function Navbar() {
   return (
     <>
       <nav className="bg-zinc-50">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+        <div className="max-w-screen-xl flex flex-wrap items-center flex-row justify-between p-4">
           <button
             data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded="false"
             title=""
@@ -30,7 +30,7 @@ export default function Navbar() {
             </svg>
           </button>
 
-          <a className="w-[15vh]  ml-[5vh]">
+          <a className="xl:w-[15vh] w-[10vh]  ml-[5vh]">
             <Image src={logo} alt="Flowbite Logo" />
           </a>
           <div>
@@ -39,7 +39,7 @@ export default function Navbar() {
             </button>
           </div>
           <div
-            className="hidden w-full md:block md:w-auto ml-[25vh]" // Add the flex-row-reverse class here
+            className="hidden w-full lg:block md:w-auto ml-[25vh]" // Add the flex-row-reverse class here
             id="navbar-default"
           >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
