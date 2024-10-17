bd982666147c052043dc99d009e3a627fba1de23
koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2024-10-17 08:34:18 +0530

    💄 Update loading page style

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-3xl font-bold text-black text-opacity-80 lg:text-5xl">
+        Loading...
+      </div>
+    </div>
+  );
 }
